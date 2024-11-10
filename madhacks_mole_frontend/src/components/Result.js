effcb41234038ce688a0e713bb7aec7962c4bc87
young <hyangyoungs@gmail.com>
2024-11-10 11:06:26 -0600

    fix: normal and abnormal logic

--- madhacks_mole_frontend/src/components/Result.js
+++ madhacks_mole_frontend/src/components/Result.js
@@ -10,7 +10,6 @@ const name = {
 };
 
 function Result() {
-
     const location = useLocation();
     const navigate = useNavigate();
     const { prediction, imageUrl } = location.state || {}; // Access state passed via navigate
@@ -22,6 +21,10 @@ function Result() {
     const handleNewUpload = () => {
         navigate("/");
     };
+
+    const hasNormal = prediction.some(
+        (item) => item.class === "NV" && item.score >= 0.6
+    );
 
     return (
         <div id="result" className="flex flex-col items-center">
@@ -36,23 +39,25 @@ function Result() {
             )}
 
             <ul style={{ listStyleType: "none" }}>
-                {/* Check if any prediction item is abnormal */}
-                {prediction.some(
+                {/* Show abnormal only if no normal condition is found */}
+                {!hasNormal && prediction.some(
                     (item) => item.class !== "NV" && item.score < 0.6
                 ) && (
                     <li className="text-[#3F414E] text-[2rem] font-bold text-center py-3 mb-2">
                         Abnormal
                         <p className="text-[#797A7C] text-[1rem] font-normal">
-                            Uh-oh, partner! This mole’s looking suspicious. We need to dig deeper and investigate further.
+                            Uh-oh, partner! This mole’s looking suspicious. We need to dig deeper and investigate
+                            further.
                         </p>
                     </li>
                 )}
 
                 {/* Map through predictions and show abnormal items */}
-                {prediction.map((item, index) => (
+                {!hasNormal && prediction.map((item, index) => (
                     <li key={index} className="mb-1">
                         {item.class !== "NV" ? (
-                            <div className="grid grid-cols-2 items-center px-4 text-[#3F414E] text-[1rem] font-semibold">
+                            <div
+                                className="grid grid-cols-2 items-center px-4 text-[#3F414E] text-[1rem] font-semibold">
                                 {/* Progress Bar */}
                                 {name[item.class]}
 
@@ -61,8 +66,9 @@ function Result() {
                                         className="h-5 rounded-full bg-[#6F6662]"
                                         style={{ width: `${item.score * 100}%` }}
                                     ></div>
-                                    <span className="absolute inset-0 flex items-center justify-center text-sm font-medium text-gray-300">
-                                    {(item.score * 100).toFixed(2)}%
+                                    <span
+                                        className="absolute inset-0 flex items-center justify-center text-sm font-medium text-gray-300">
+                                        {(item.score * 100).toFixed(2)}%
                                     </span>
                                 </div>
                             </div>
@@ -71,14 +77,13 @@ function Result() {
                 ))}
 
                 {/* Show normal condition if found */}
-                {prediction.some(
-                    (item) => item.class === "NV" && item.score >= 0.6
-                ) && (
+                {hasNormal && (
                     <li className="text-[#3F414E] text-[2rem] font-bold text-center py-2">
                         Normal
                         <p className="text-[#797A7C] text-[1rem] font-normal">
                             Ah, excellent news, partner! <br />
-                            It looks like this mole is nothing to worry about. Case closed, no danger here. But remember, I’ll always be here to keep an eye out for you.
+                            It looks like this mole is nothing to worry about. Case closed, no danger here. But
+                            remember, I’ll always be here to keep an eye out for you.
                         </p>
                     </li>
                 )}
@@ -90,15 +95,17 @@ function Result() {
 
             <div className="flex flex-col gap-2 w-full">
                 <Link to="/description">
-                    <button className="font-semibold bg-[#31211C] rounded-[2.3rem] text-[1rem] text-[#fff] py-4 hover:bg-transparent hover:text-[#31211C] border border-[#31211C] w-full">
+                    <button
+                        className="font-semibold bg-[#31211C] rounded-[2.3rem] text-[1rem] text-[#fff] py-4 hover:bg-transparent hover:text-[#31211C] border border-[#31211C] w-full">
                         Learn More
                     </button>
                 </Link>
-                <Link to="/description">
-                    <button className="font-semibold bg-[#6F6662] hover:bg-[#31211C] rounded-[2.3rem] text-[1rem] text-[#fff] py-4 hover:bg-transparent hover:text-[#31211C] border border-[#31211C] w-full">
-                        Share Your Findings
-                    </button>
-                </Link>
+                <button
+                    data-app="share_buttons"
+                    data-link="https://blog.example.com/"
+                    className="font-semibold bg-[#6F6662] hover:bg-[#31211C] rounded-[2.3rem] text-[1rem] text-[#fff] py-4 hover:bg-transparent hover:text-[#31211C] border border-[#31211C] w-full">
+                    Share Your Findings
+                </button>
             </div>
         </div>
     );
